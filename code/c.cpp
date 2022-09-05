0146f20d12ab08cc455870458a53ac22a39cd4ae
zhanghaoran <2017929959@qq.com>
2022-09-05 14:02:34 +0800

    ""

--- code/c.cpp
+++ code/c.cpp
@@ -1,6 +1,42 @@
 #include "bits/stdc++.h"
-using namespace std;
-int main() {
 
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    int a[200010];
+    int b[200010];
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    for (int i = 1; i <= n; i++)
+        cin >> b[i];
+    a[n + 1] = a[1];
+    b[n + 1] = b[1];
+    for (int i = 1; i <= n; i++) {
+        if (a[i] == b[i])
+            continue;
+        if (a[i] > b[i]) {
+            cout << "no" << endl;
+            return;
+        }
+        if (b[i + 1] + 1 < b[i]) {
+            cout << "no" << endl;
+            return;
+        }
+    }
+    cout << "yes" << endl;
+}
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
     return 0;
 }
+/*
+5
+4 3 4 3 1
+4 3 4 3 2
+ * */
